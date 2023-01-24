c9101dc35ed5a4136cda7eb65a5467e77399435a
Jose Murilo <murilojucas360@hotmail.com>
2023-01-24 12:09:55 -0300

    add project complete

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,28 +1,33 @@
 import { useState, useEffect} from 'react';
-import { GrSearch } from 'react-icons/gr';
 import { BsThermometerHigh } from 'react-icons/bs'
-import { WeatherCondition } from '../components/WeatherCondition'
+import { GoSearch } from 'react-icons/go';
+import { ClimateCard } from '../components/ClimateCard';
+import { ForecastCard } from '../components/ForecastCard'
 import './style.css'
 
 export function App() {
-
-  const [text, setText] = useState('')
   const [searchedCity, setSearchedCity] = useState('jucas')
+  const [inputCity, setInputCity] = useState('')
   const [weatherData, setWeatherData] = useState(null)
-  const [nowDate, setNowDate] = useState()
-  
+  const [dateNow, setDateNow] = useState()
+
   const API = `https://api.weatherapi.com/v1/forecast.json?key=fb85b303e1fe4286a2b15407223112&q=${searchedCity}&days=4&lang=pt`
 
   async function getCityWeather() {
     const response = await fetch(API)
-    
-    if (response.status === 200) {
-      const data = await response.json();
-      setWeatherData(data)
-    } else if (response.status === 400) {
+    console.log(response);
+     if (response.status == 200) {
+       const data = await response.json();
+       setWeatherData(data)
+     } else if (response.status == 400){
       alert("Cidade não encontrada")
-    }
-  }
+     }
+   }
+
+   function searchCity(event) {
+    event.preventDefault();
+    setSearchedCity(inputCity)
+   }
 
   useEffect(() => {
     getCityWeather()
@@ -30,82 +35,95 @@ export function App() {
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setNowDate(new Date().toLocaleString())
+      setDateNow(new Date().toLocaleString())
     }, 1000)
 
-    return () => clearInterval(timer)
-  })
+    return () => {
+      clearInterval(timer)
+    }
 
-  const handleChangeClick = () => {
-    setSearchedCity(text);
-  }
-
-  const handleChange = (event) => {
-    setText(event.target.value)
-  }
+  }, [])
 
   return (
     <div className="container">
-      <div className="containerApp">
-        <header>
-          <h1>Previsão do Tempo</h1>
-          <form action="">
-            <label htmlFor="citySearchInput" className='srOnly'>Pesquisar nome da cidade</label>
-            <input type="text" placeholder='Nome da cidade' id='citySearchInput' onChange={handleChange}/>
-            <GrSearch className='searchIcon' />
-            <button onClick={(event) => {
-              event.preventDefault()
-              handleChangeClick()
-            } } className='searchButton'>Buscar</button>
-          </form>
-        </header>
-        <main>
-          <p></p>
-          <p></p>
-          {
-            searchedCity && weatherData && (
-              <article> 
-            <section className='blockCityName'>
-              <h2>{weatherData.location.name}, {weatherData.location.region}</h2>
-              <p>{weatherData.location.country}, {nowDate}</p>
-            </section>
-            <section className='blockCurrentTime'>
-              <div className="currentTime">
-                <div className="blockDegree">
-                  <BsThermometerHigh className='iconThermometer' />
-                  <p className='degreeCurrent'>{weatherData.current.temp_c}°</p>
-                  <p className='degreeMaxMin'>
-                    <span className="degreeMax">{weatherData.forecast.forecastday[0].day.maxtemp_c}°</span>
-                    <span className="degreeMin">{weatherData.forecast.forecastday[0].day.mintemp_c}°</span>
-                  </p>
+        <div className="container-app">
+          <header>
+            <h1>Previsão do Tempo</h1>
+            <form action="" onSubmit={searchCity}>
+              <label htmlFor="citySearchInput" className='srOnly'>Pesquisar nome da cidade</label>
+              <input 
+                type="text" 
+                placeholder='Nome da cidade' 
+                id='citySearchInput'
+                onChange={(event) => setInputCity(event.target.value)}
+              />
+              <GoSearch className='searchIcon' />
+              <button className='searchButton'>Buscar</button>
+            </form>
+          </header>
+
+          <main>
+            {
+              searchedCity && weatherData && (
+                <article>
+              <section className='blockCityName'>
+                <h2>{weatherData.location.name}, {weatherData.location.region}</h2>
+                <p>{weatherData.location.country}, {dateNow}</p>
+              </section>
+              <section className='blockCurrentTime'>
+                <div className="currentTime">
+                  <div className="blockDegree">
+                    <BsThermometerHigh className='iconThermometer' />
+                    <p className='degreeCurrent'>{weatherData.current.temp_c}°</p>
+                    <p className='degreeMaxMin'>
+                      <span className="degreeMax">{weatherData.forecast.forecastday[0].day.maxtemp_c}°</span>
+                      <span className="degreeMin">{weatherData.forecast.forecastday[0].day.mintemp_c}°</span>
+                    </p>
+                  </div>
+                  <div className="blockSituation">
+                    <img src={weatherData.current.condition.icon} alt="icon" />
+                      <div>
+                        <p>{weatherData.current.condition.text}</p>
+                        <p>Sensação Terminal {weatherData.current.feelslike_c}°</p>
+                      </div>
+                  </div>
                 </div>
-                <div className="blockSituation">
-                  <img src={weatherData.current.condition.icon} alt="icon" />
-                    <div>
-                      <p>{weatherData.current.condition.text}</p>
-                      <p>Sensação Terminal {weatherData.current.feelslike_c}°</p>
-                    </div>
-                </div>
-              </div>
-            </section>
-            <section className='containerWeatherCondition'>
-              <WeatherCondition climate='Vento' condition={`${weatherData.current.wind_kph}km/h`} />
-              <WeatherCondition climate='Umidade' condition={`${weatherData.current.wind_kph}%`} />
-              <WeatherCondition climate='Chuva' condition={`${weatherData.current.wind_kph}mm`} />
-            </section>
-            <section className='containerWeatherForecast'>
-              <ol>
-                <li>Component "Map"</li>
-              </ol>
-            </section>
-              </article>
-            )
-          }
-        </main>
-        <footer>
-          <p>Web Developer Course - Júcas</p>
-        </footer>
-      </div>
+              </section>
+              <section className='containerWeatherCondition'>
+                <ClimateCard climate='Vento' condition={`${weatherData.current.wind_kph}km/h`}  />
+                <ClimateCard climate='Umidade' condition={`${weatherData.current.humidity}%`} />
+                <ClimateCard climate='Chuva' condition={`${weatherData.current.precip_mm}mm`} />
+              </section>
+              <section className='containerWeatherForecast'>
+                <ol>
+                  {
+                    weatherData.forecast.forecastday.map((forecastDay, index) => {
+                      return (
+                        <li key={index}>
+                          <ForecastCard 
+                            day={
+                              index == 0 ? 'Hoje' :
+                              Intl.DateTimeFormat('pt-BR', {weekday: 'long'})
+                              .format(new Date(forecastDay.date.split('-').join('/')))
+                            }
+                            icon={forecastDay.day.condition.icon}
+                            tempMax={forecastDay.day.maxtemp_c}
+                            tempMin={forecastDay.day.mintemp_c}
+                          />
+                        </li>
+                      )})
+                  }
+
+                </ol>
+              </section>
+                </article>
+              )
+            }
+          </main>
+          <footer>
+            <p>Web Developer Course - Jucás</p>
+          </footer>
+        </div>
     </div>
   )
 }
---
+++ src/components/ClimateCard/index.jsx
@@ -0,0 +1,10 @@
+import './style.css'
+
+export function ClimateCard({ climate, condition }) {
+    return (
+        <div className='weatherCondition'>
+            <h4>{condition}</h4>
+            <p>{climate}</p>
+        </div>
+    )
+}
